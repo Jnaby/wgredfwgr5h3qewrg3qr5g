02ff5b59f07889443a88fc5081ac4b818cc05295
Jnaby <36682528+Jnaby@users.noreply.github.com>
2018-10-06 02:16:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "J";
+const prefix = "1";
 /////////////////////////
 ////////////////////////
 
